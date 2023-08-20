d7f33e4452b43b930a3c6be49d51aff6f86b588a
Rakshya Panday <rakshyapanday202@gmail.com>
2023-08-19 21:53:49 -0700

    PlayNow

--- component/PlayNow.js
+++ component/PlayNow.js
@@ -3,6 +3,12 @@ import '../css/playnow.css';
 const PlayNow = ()=>{
     const arr = [1,2,3,4,5,6,7];
     const[selectnum,setselectnum] = useState();
+    const[currentDice,setCurrentDice] = useState();
+    const generateRandomNumber = (min,max) => {
+        return Math.floor( Math.random() * (max - min) + min);
+
+    };
+
     
     return(
         <> 
@@ -23,9 +29,15 @@ const PlayNow = ()=>{
             })}
             <p>Select Number</p>
          </div>
-            
-
-        </div>
+         </div>
+         <div className='dice2'  onClick = {()=>generateRandomNumber(1,6)}>
+           
+            <img src='dice2.jpg' alt='dice2'/>
+            <p>Click on dice to roll</p>
+         <button>Reset Score</button>
+         <button>Show Rules</button> 
+         </div>
+         
         </>
     )
 }
